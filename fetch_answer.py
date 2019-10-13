ebec7d7a77f3be8fcd66733454bf7932bb7259e5
Akash Mondal <akashmondal1810@gmail.com>
2019-10-13 10:13:36 +0530

    Update fetch_answer.py

--- fetch_answer.py
+++ fetch_answer.py
@@ -8,7 +8,7 @@ from gensim.models.keyedvectors import KeyedVectors
 from word2vec_repo.DocSim import DocSim
 from nltk.corpus import stopwords
 
-#some of the code are from https://stackoverflow.com/a/8897648 and https://github.com/v1shwa/document-similarity/blob/master/example.py
+#some of the code are from https://stackoverflow.com/a/8897648 
 
 model_path = 'word2vec_repo/model.bin'
 stopwords = stopwords.words('english')
@@ -20,15 +20,19 @@ ds = DocSim(model, stopwords=stopwords)
 def get_doc(qu):
     final_docs = matching_score(10, qu)
     #print(final_docs)
+    #print(final_docs)
+    nd= len(final_docs)
+    if len(final_docs)>3:
+        nd = 3
     answer_doc = []
-    for i in range(3):
+    for i in range(nd):
         file = open(final_docs[i], 'r', encoding='cp1250')
         text = file.read().strip()
         file.close()
         text=text.replace('\n', ' ')
         tl = nltk.sent_tokenize(text)
-        state = str(final_docs[i])[37:]
-        tl = [state[:-4] +". " +line for line in tl]
+        state = str(final_docs[i]).split('/')
+        tl = [str(state[len(state)-1])[:-4] +". " +line for line in tl]
         answer_doc+=tl
 
     answer_doc.append(qu)
@@ -67,8 +71,25 @@ print("----------------------------------")
 print("w2v: ", get_ans_w2v(q, ans_lists))
 
 import pandas as pd
+import time
 df = pd.read_csv(r"documents/Question.csv",encoding='latin-1')
 df["ans_tfIdf"]= [get_answer(q, get_doc(q)) for q in df['Question']]
+
+time_tf = []
+for q in df['Question']:
+    start_time = time.time()
+    get_answer(q, get_doc(q))
+    time_tf.append((time.time() - start_time))
+df["exe_time_tfIdf"]= time_tf
+
 df["ans_w2v"]= [get_ans_w2v(q, get_doc(q)) for q in df['Question']]
+
+time_w2v = []
+for q in df['Question']:
+    start_time = time.time()
+    get_ans_w2v(q, get_doc(q))
+    time_w2v.append((time.time() - start_time))
+df["exe_time_w2v"]= time_w2v
+
 df.to_csv('documents/Question_&_ans_genrtd.csv', index=False)
 '''
